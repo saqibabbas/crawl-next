af97f95ee1d0f120fbfa8460f1d687fc647f85e9
Maaz Anzar <maazanzar@gmail.com>
2019-12-19 00:03:02 +0500

    cHANGES

---
+++ crawl-next/crawl-next/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crawl_next.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string url { get; set; }
+
+        public string html { get; set; }
+
+    }
+}
--- crawl-next/crawl-next/Program.cs
+++ crawl-next/crawl-next/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using crawl_next.Data_Context;
 using HtmlAgilityPack;
 
 namespace crawl_next
@@ -13,30 +14,31 @@ namespace crawl_next
     {
         static void Main(string[] args)
         {
-            startCrawlerasync();
-        }
+            //startCrawlerasync();
 
-        private static async Task startCrawlerasync()
-        {
-            try
+            List<string> questionUrl = new List<string>();
+            List<string> tags = Crawl_Helper.getTags();
+
+            foreach (var item in tags)
             {
-                var url = "https://stackoverflow.com/";
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-                request.Method = "GET";
-                request.ContentType = "application/json";
-                WebResponse response = request.GetResponse();
-                var responseString = new
-                StreamReader(response.GetResponseStream()).ReadToEnd();
+                questionUrl=  Crawl_Helper.getQuestions(string.Format(Crawl_Helper.questionsUrl, item, "votes", 1, 50));
 
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(responseString);
+                foreach (var itemquestion in questionUrl)
+                {
+                    Console.WriteLine(itemquestion);
+                }
 
             }
-            catch (Exception ex)
-            {
-                Console.Write(ex);
-            }
 
+            //questionUrl = Crawl_Helper.getQuestions("https://stackoverflow.com/questions/tagged/c%23?tab=votes&pagesize=50");
+
+
+            //Crawl_Helper.getQuestionDetials("https://stackoverflow.com/questions/7074/what-is-the-difference-between-string-and-string-in-c");
+            //DataContext db = new DataContext();
+            //foreach (var item in db.documents.Select(x => x.url))
+            //{
+            //    Console.WriteLine(item);
+            //} 
         }
     }
 }
